fb6d7400bd39c8a208b360c06d9d078178b67f2c
Stephen <haugland98@gmail.com>
2020-12-14 03:30:31 -0800

    Tests Updated

--- UITest.py
+++ UITest.py
@@ -40,31 +40,56 @@ def main():
     loginButton = driver.find_element_by_css_selector("[type=submit]")
     loginButton.click()
 
-    # save a screenshot to verify successful login
-    driver.save_screenshot("test1.png")
+    # # save a screenshot to verify successful login
+    # driver.save_screenshot("test1.png")
 
 #### Step 3: Begin UI tests ####
 
     ## Test 01_AddFirstDaily
+    # verify that there are no dailies currently being tracked 
+    DailyTextField = driver.find_element_by_css_selector('[placeholder="Add a Daily"]')
+    DailyTextField.send_keys("floss")
+    # save a screenshot to verify task successfully added
+    driver.save_screenshot("test1.png")
 
     ## Test 02_AddSecondDaily
+    DailyTextField.clear()
+    DailyTextField.send_keys("Read")
+    # save a screenshot to verify task successfully added
+    driver.save_screenshot("test2.png")
 
     ## Test 03_AddThirdDaily
-
+    DailyTextField.clear()
+    DailyTextField.send_keys("Make Bed")
+    driver.save_screenshot("test3.png")
     ## Test 04_CheckFirstDaily
-
+    FirstCheckBox = driver.find_element_by_css_selector('[class="task-control daily-todo-control task-better-control-inner-daily-todo"]')
+    FirstCheckBox.click()
+    driver.save_screenshot("test4.png")
     ## Test 05_UncheckFirstDaily
-
+    FirstCheckBox.click()
+    driver.save_screenshot("test5.png")
     ## Test 06_CheckAndUncheckFirstDailyOnce
-
+    FirstCheckBox.click()
+    FirstCheckBox.click()
+    driver.save_screenshot("test6.png")
     ## Test 07_CheckAndUncheckFirstDailyTenTimes
-
+    for x in range(10):
+        FirstCheckBox.click()
+        FirstCheckBox.click()
+    driver.save_screenshot("test7.png")
     ## Test 08_DeleteFirstDaily
-
+    FirstDeleteButton = driver.find_element_by_css_selector('[class="habitica-menu-dropdown-toggle"]')
+    FirstDeleteButton.click()
+    driver.save_screenshot("test8.png")
     ## Test 09_DeleteSecondDaily
-
+    SecondDeleteButton = driver.find_element_by_css_selector('[class="habitica-menu-dropdown-toggle"]')
+    SecondDeleteButton.click()
+    driver.save_screenshot("test9.png")
     ## Test 10_DeleteLastDaily
-
+    ThirdDeleteButton = driver.find_element_by_css_selector('[class="habitica-menu-dropdown-toggle"]')
+    ThirdDeleteButton.click()
+    driver.save_screenshot("test10.png")
     ## Fuzz testing for text search box ##
     # naughty strings
     # queries
